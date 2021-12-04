7ac3113459fc00fe25a7cde3ef6bb341a2e0b7e9
cctorres <c.torresalzate@gmail.com>
2021-12-04 08:24:32 -0500

    nuevos precios

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "../style/Header.css"
 import logo from "../assets/logoMariseta.svg"
+import logoShirt from "../assets/logoMarisetaTShirt.svg"
 
 const Header = () => {
     return (
         <div className="header-container">
             <img src={logo} alt="mariseta-logo" className="header--logo" />
-            <p>Mariseta</p>
+            <img src={logoShirt} alt="mariseta-logo-shirt" className="mariseta-logo-shirt" />
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,65 +1,51 @@
 import React from 'react'
 import '../style/Home.css'
-import Lottie from 'react-lottie-player'
-import xmas from '../assets/xmas.json'
-import snowfall from '../assets/snowfall.json'
-
+import wppLogo from '../assets/wpp.svg'
 
 const Home = () => {
 
-  const wpp = "Presiona aquí para realizar tus pedidos. Con gusto te atenderemos."
+  const wpp = "Presiona aquí para realizar su pedido"
   const navidad = "Catalogo de Navidad";
-  const catalogoDamas13500 = "Catalogo de Dama $13500";
-  const catalogoDamas16500 = "Catalogo de Dama $16500";
-  const catalogoDamas14500 = "Catalogo de Dama Ribetes $14500";
-  const catalogoDamas19000 = "Catalogo de Dama Manga Doblada $19000";
-  const catalogoCaballero19000 = "Catalogo de Caballero $19000";
-  const catalogoNinos = "Catalogo de Niños"
+  const catalogoDamasBasica = "Catalogo de Dama $15500";
+  const catalogoDamasAlta = "Catalogo de Dama $18500";
+  const catalogoDamasRibete = "Catalogo de Dama Ribetes $16500";
+  const catalogoDamasMangas = "Catalogo de Dama Manga Doblada $21000";
+  const catalogoCaballero = "Catalogo de Caballero $21000";
+  const catalogoNinos = "Catalogo Infantil $18500"
 
   return (
     <div className="home-container">
-      <div className="home-side"><Lottie
-        animationData={xmas}
-        play
-        style={{ width: 400, height: 400 }}
-      /></div>
       <div className="catalogo-container">
-        <div className="mobileLottieContainer"><Lottie
-          animationData={xmas}
-          play
-          style={{ width: 100, height: 100 }}
-        /></div>
         <div className="catalogowpp">
-          <a href="https://wa.me/573003193035?text=Hola!%20me%20interesa%20sus%20servicios" target="_blank" rel="noopener noreferrer" className="catalogo-a">{wpp}</a>
+          <a href="https://wa.me/573003193035?text=Hola!%20me%20interesa%20sus%20servicios" target="_blank" rel="noopener noreferrer" className="catalogo-a">
+            <div className="catalogo-wpp">
+              <p>{wpp}</p>
+              <img src={wppLogo} alt="whatsapp" className="wpp-logo" />
+            </div>
+          </a>
         </div>
         <div className="catalogoNavidad">
           <a href="https://drive.google.com/file/d/1afA650qv4G2Zt2QlMcHYcqhtGuSJZ5pZ/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{navidad}</a>
         </div>
-        <div className="catalogoDamas13500">
-          <a href="https://drive.google.com/file/d/1rW6mgKoCcZwoIonRt_QM6rsLT6OAagnl/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamas13500}</a>
+        <div className="catalogoDamasBasica">
+          <a href="https://drive.google.com/file/d/1rW6mgKoCcZwoIonRt_QM6rsLT6OAagnl/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamasBasica}</a>
         </div>
-        <div className="catalogoDamas16500">
-          <a href="https://drive.google.com/file/d/1L_Odfa-1KjzLOq4ini0B3NTuzMg5coQQ/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamas14500}</a>
+        <div className="catalogoDamasAlta">
+          <a href="https://drive.google.com/file/d/1L_Odfa-1KjzLOq4ini0B3NTuzMg5coQQ/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamasAlta}</a>
         </div>
-        <div className="catalogoDamas14500">
-          <a href="https://drive.google.com/file/d/1K7lcADhMlmQ9MpRkGUDMkQ04NhDjtvnM/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamas16500}</a>
+        <div className="catalogoDamasRibete">
+          <a href="https://drive.google.com/file/d/1K7lcADhMlmQ9MpRkGUDMkQ04NhDjtvnM/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamasRibete}</a>
         </div>
-        <div className="catalogoDamas19000">
-          <a href="https://drive.google.com/file/d/1XsliVrT79VCGbx55dOCQCUA8CDvOt99a/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamas19000}</a>
+        <div className="catalogoDamasMangas">
+          <a href="https://drive.google.com/file/d/1XsliVrT79VCGbx55dOCQCUA8CDvOt99a/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoDamasMangas}</a>
         </div>
-        <div className="catalogoCaballero19000">
-          <a href="https://drive.google.com/file/d/1b8vvHFnPSTrISRbObD-jpjWAW9ohSwLr/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoCaballero19000}</a>
+        <div className="catalogoCaballero">
+          <a href="https://drive.google.com/file/d/1b8vvHFnPSTrISRbObD-jpjWAW9ohSwLr/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoCaballero}</a>
         </div>
         <div className="catalogoNinos">
           <a href="https://drive.google.com/file/d/1gPSMIUqf_ziBU8jU2L7qasGKwy6FZ06c/view" target="_blank" rel="noopener noreferrer" className="catalogo-a">{catalogoNinos}</a>
         </div>
       </div>
-      <div className="home-side"><Lottie
-        loop
-        animationData={snowfall}
-        play
-        style={{ width: 400, height: 400 }}
-      /></div>
     </div>
   )
 }
