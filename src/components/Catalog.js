f2d45cf5ea5783c948099f55bc4ec62e8d03f876
cctorres <c.torresalzate@gmail.com>
2022-04-16 08:51:11 -0500

    firebase init build

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -3,7 +3,6 @@ import React from 'react'
 import '../style/Catalog.css'
 
 const Catalog = (props) => {
-    console.log(props)
   return (
     <div className="catalog">
           <a href={props.catalogsInfo.link} target="_blank" rel="noopener noreferrer" className="catalog-a">{props.catalogsInfo.text}</a>
